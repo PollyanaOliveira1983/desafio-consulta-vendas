929e16d3fd2f22af665b94428a4740790e3c05c8
Pollyana <pollyana.oliveira@gmail.com>
2022-12-14 15:56:56 -0300

    Atualizacao teste

--- src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -1,13 +1,18 @@
 package com.devsuperior.dsmeta.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
+import com.devsuperior.dsmeta.dto.SaleRepoDTO;
+import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
 import com.devsuperior.dsmeta.services.SaleService;
 
 @RestController
@@ -23,15 +28,25 @@ public class SaleController {
 		return ResponseEntity.ok(dto);
 	}
 
-	@GetMapping(value = "/report")
-	public ResponseEntity<?> getReport() {
-		// TODO
-		return null;
-	}
-
 	@GetMapping(value = "/summary")
-	public ResponseEntity<?> getSummary() {
-		// TODO
-		return null;
+	public ResponseEntity<List<SaleSummaryDTO>> getSummary(
+				@RequestParam(name = "minDate", defaultValue = "")  String minDate,
+				@RequestParam(name = "maxDate", defaultValue = "") String maxDate)
+	{
+
+		List<SaleSummaryDTO> dto = service.getSummary(minDate, maxDate);
+
+		return ResponseEntity.ok(dto);
+	}
+	
+	@GetMapping(value = "/report")
+	public ResponseEntity<List<SaleRepoDTO>> getReport(
+			@RequestParam(name = "minDate", defaultValue = "")  String minDate,
+			@RequestParam(name = "maxDate", defaultValue = "") String maxDate,
+			@RequestParam(name= "name", defaultValue = "") String nameSale){
+
+		List<SaleRepoDTO> dto = service.getReport(minDate, maxDate, nameSale);
+
+		return ResponseEntity.ok(dto);
 	}
 }
